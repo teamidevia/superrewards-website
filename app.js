d9a32795209b9f0e20a790d89c5a564ba3bac77e
Mortuz A <mortuzalam@gmail.com>
2023-09-19 21:17:26 +0530

    fixes

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-const button = document.querySelector('.featuresButton');
-const contentPart1 = document.querySelector('.part1');
-const contentPart2 = document.querySelector('.part2');
-const contentPart3 = document.querySelector('.part3');
+const button = document.querySelector('.featuresButton')
+const contentPart1 = document.querySelector('.part1')
+const contentPart2 = document.querySelector('.part2')
+const contentPart3 = document.querySelector('.part3')
 
 button.addEventListener('mouseover', (e) => {
-    // console.log(e.target);
-    let lastNumber = e.target.innerText[e.target.innerText.length - 1];
-    if (lastNumber == 1) {
-        contentPart1.classList.add('visible');
-        contentPart2.classList.add('invisible');
-        contentPart3.classList.add('invisible');
-        contentPart2.classList.remove('visible');
-        contentPart3.classList.remove('visible');
-        contentPart1.classList.remove('invisible');
-    }
-    if (lastNumber == 2) {
-        contentPart2.classList.add('visible');
-        contentPart1.classList.add('invisible');
-        contentPart3.classList.add('invisible');
-        contentPart1.classList.remove('visible');
-        contentPart3.classList.remove('visible');
-        contentPart2.classList.remove('invisible');
-    }
-    if (lastNumber == 3) {
-        contentPart3.classList.add('visible');
-        contentPart2.classList.add('invisible');
-        contentPart1.classList.add('invisible');
-        contentPart2.classList.remove('visible');
-        contentPart1.classList.remove('visible');
-        contentPart3.classList.remove('invisible');
-    }
+  // console.log(e.target);
+  let lastNumber = e.target.innerText[e.target.innerText.length - 1]
+  if (lastNumber == 1) {
+    contentPart1.classList.add('d-block')
+    contentPart2.classList.add('d-none')
+    contentPart3.classList.add('d-none')
+    contentPart2.classList.remove('d-block')
+    contentPart3.classList.remove('d-block')
+    contentPart1.classList.remove('d-none')
+  }
+  if (lastNumber == 2) {
+    contentPart2.classList.add('d-block')
+    contentPart1.classList.add('d-none')
+    contentPart3.classList.add('d-none')
+    contentPart1.classList.remove('d-block')
+    contentPart3.classList.remove('d-block')
+    contentPart2.classList.remove('d-none')
+  }
+  if (lastNumber == 3) {
+    contentPart3.classList.add('d-block')
+    contentPart2.classList.add('d-none')
+    contentPart1.classList.add('d-none')
+    contentPart2.classList.remove('d-block')
+    contentPart1.classList.remove('d-block')
+    contentPart3.classList.remove('d-none')
+  }
 })
+
+const ctaSection = document.querySelector('.cta-area')
+const afterCtaSection = document.querySelector('.after-cta')
+
+const fixForCtaTranslate = () => {
+  afterCtaSection &&
+    (afterCtaSection.style.paddingTop = ctaSection.offsetHeight / 2 + 'px')
+}
+window.addEventListener('resize', fixForCtaTranslate)
+window.addEventListener('orientationchange', fixForCtaTranslate)
+fixForCtaTranslate()
